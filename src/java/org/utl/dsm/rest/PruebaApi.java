1811ce24942b530712d1dd8736cbc777ea874526
OsvaldoGM(El rogon) <Karypekito@gmail.com>
2025-06-19 00:06:18 -0600

    Initial commit

---
+++ src/java/org/utl/dsm/rest/PruebaApi.java
@@ -0,0 +1,24 @@
+
+package org.utl.dsm.rest;
+import jakarta.ws.rs.DefaultValue;
+import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.Application;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import org.utl.dsm.control.empleado;
+@Path("prueba")
+public class PruebaApi extends Application{
+        @Path("saludar")
+    @Produces(MediaType.APPLICATION_JSON)
+    @GET
+    public Response saludar()
+    {
+        String out =  """
+                      {"result":"Hola Mundo"}
+                      """;
+        return Response.status(Response.Status.OK).entity(out).build();
+    }
+}
