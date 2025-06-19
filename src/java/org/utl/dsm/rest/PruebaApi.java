472c0fa1ad45012bec6c6c35a44b2c310f826b4e
OsvaldoGM(El rogon) <Karypekito@gmail.com>
2025-06-19 00:28:31 -0600

    Agregadas carpetas vacías js/ y css/ con .gitkeep

--- src/java/org/utl/dsm/rest/PruebaApi.java
+++ src/java/org/utl/dsm/rest/PruebaApi.java
@@ -8,7 +8,6 @@ import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.Application;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
-import org.utl.dsm.control.empleado;
 @Path("prueba")
 public class PruebaApi extends Application{
         @Path("saludar")
